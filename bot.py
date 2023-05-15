f0526ba4324afdcb947622bafb43c8b3708df2aa
cristobers <72763707+cristobers@users.noreply.github.com>
2023-05-15 21:01:20 +0100

    cleaned up bot

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import discord, datetime
+import discord, datetime, asyncio
 from discord.ext import commands
 from discord.ui import Button, View
 
@@ -6,13 +6,13 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix = ']', intents=intents)
 
 with open("TOKEN.token", "r") as f:
-    token = f.readlines()[0]
-    
+    token = next(iter(f.readlines()))
+
 @bot.tree.command(name="findfamiliar")
 @discord.app_commands.checks.has_permissions(administrator=True)
 async def findfamiliar(interaction: discord.Interaction, user: str = None, reason: str = None):
     if not interaction.user.guild_permissions.administrator:
-        return await interaction.response.send_message("Nope!")
+        return await interaction.response.send_message("🍅")
     if user is None:
         return await interaction.response.send_message("You didn't specify a user.")
     if reason is None:
@@ -24,13 +24,13 @@ async def findfamiliar(interaction: discord.Interaction, user: str = None, reaso
     for member in discordServerGuild.members:
         if user.lower() in member.name.lower():
             similarUsers.append(member)
-        embed = discord.Embed(color=0xec0000)
 
     if len(similarUsers) != 0:
-        embed.title=f"Users with similar names to `{user}` found:"
+        embed = discord.Embed(color=0xec0000)
+        embed.title=f"Users containing `{user}`:"
         embed.timestamp = datetime.datetime.now()
     else:
-        embed.title=f"No users found"
+        embed.title="No users found"
         return await interaction.response.send_message(embed=embed)
     for user in similarUsers:
         embed.add_field(name="", value=user.mention, inline=False)
@@ -46,17 +46,20 @@ async def findfamiliar(interaction: discord.Interaction, user: str = None, reaso
             button.label = "Users kicked."
             button.emoji="🤯"
             button.disabled = True
-            log += f"## Kicking log ##\nReason for kick: {reason}\n--------\n"
             for user in similarUsers:
                 try:
-                    log += f"{user}\n"
                     await user.kick(reason=str(reason))
+                    print(f"Kicking {user}")
                 except Exception as e:
                     log += f"error kicking {user}, reason: {e}"
                     continue 
             count = len(similarUsers)
             similarUsers.clear()
-            await interaction.response.edit_message(content=f"`{count}` Users kicked ```{log}```", view=self)
+
+            if len(log) == 0:
+                return await interaction.response.edit_message(content=f"`{count} users kicked`", view=self)
+            return await interaction.response.edit_message(content=f"`{count} users kicked`\n```{log}```", view=self)
+
     view = buttonView()
     return await interaction.response.send_message(embed=embed, view=view)
 
@@ -64,5 +67,4 @@ async def findfamiliar(interaction: discord.Interaction, user: str = None, reaso
 async def on_ready():
     print("hello!")
     synced = await bot.tree.sync()
-
 bot.run(token)
