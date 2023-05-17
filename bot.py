6eeb8bd7e46183744f3f23aaf933ebc425165195
cristobers <72763707+cristobers@users.noreply.github.com>
2023-05-17 18:30:00 +0100

    moved kicking to an async function

--- bot.py
+++ bot.py
@@ -8,6 +8,22 @@ bot = commands.Bot(command_prefix = ']', intents=intents)
 with open("TOKEN.token", "r") as f:
     token = next(iter(f.readlines()))
 
+async def kick_users(similarUsers: list):
+    count, timeout = 0, 0
+    for user in similarUsers:
+        count += 1
+        try:
+            print("Time between kicks: ", timeout)
+            await user.kick(reason=str(reason))
+            await asyncio.sleep(timeout) # makes discord endpoints happier :)
+            print(f"Kicking {user}")
+            timeout = 1 << count
+        except Exception as e:
+            print("Error kicking user {user}, reason: {e}")
+            continue
+    similarUsers.clear()
+    return count
+
 @bot.tree.command(name="findfamiliar")
 @discord.app_commands.checks.has_permissions(administrator=True)
 async def findfamiliar(interaction: discord.Interaction, user: str = None, reason: str = None):
@@ -34,37 +50,22 @@ async def findfamiliar(interaction: discord.Interaction, user: str = None, reaso
         return await interaction.response.send_message(embed=embed)
     for user in similarUsers:
         embed.add_field(name="", value=user.mention, inline=False)
-    
+
     class buttonView(View):
         @discord.ui.button(label="Kick listed users", style=discord.ButtonStyle.red, emoji="💥")
         async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
-
             if not interaction.user.guild_permissions.administrator:
                 return # dont click my buttons if you aint an admin!!!
-
-            log = ''
             button.label = "Users kicked."
             button.emoji="🤯"
             button.disabled = True
-            for user in similarUsers:
-                try:
-                    await user.kick(reason=str(reason))
-                    print(f"Kicking {user}")
-                except Exception as e:
-                    log += f"error kicking {user}, reason: {e}"
-                    continue 
-            count = len(similarUsers)
-            similarUsers.clear()
-
-            if len(log) == 0:
-                return await interaction.response.edit_message(content=f"`{count} users kicked`", view=self)
-            return await interaction.response.edit_message(content=f"`{count} users kicked`\n```{log}```", view=self)
-
+            kick_users_task = asyncio.create_task(kick_users(similarUsers))
+            return await interaction.response.edit_message(content=f"😃", view=self)
     view = buttonView()
     return await interaction.response.send_message(embed=embed, view=view)
 
 @bot.event
 async def on_ready():
-    print("hello!")
+    print("bot is online! :O")
     synced = await bot.tree.sync()
 bot.run(token)
